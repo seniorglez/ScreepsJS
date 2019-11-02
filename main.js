f5c36efaf9047e3a80edc8db1ea4013f08b51453
Diego Domínguez González <d.dominguez.glez1111@gmail.com>
2019-11-03 00:33:04 +0100

    Better creep population control

--- main.js
+++ main.js
@@ -5,24 +5,47 @@ var roleRepailer = require('role.repailer');
 var roleClaimer = require('role.claimer');
 var utilities = require('utilities');
 
-var numcreeps = 20;
 let roles = ["builder", "harvester", "upgrader", "repailer"];
-
-
-
+var minNumberOfBuilders=2;
+var minNumberOfHarvesters=10;
+var minNumberOfUpgraders=4;
+var minNumberOfRepailers=2;
 
 module.exports.loop = function () {
-
+    //control de creeps number
+     var numberOfBuilders = _.sum(Game.creeps, (c) => c.memory.role == 'builder');
+     var numberOfHarvesters = _.sum(Game.creeps, (c) => c.memory.role == 'harvester');
+     var numberOfUpgraders = _.sum(Game.creeps, (c) => c.memory.role == 'upgrader');
+     var numberOfRepailers = _.sum(Game.creeps, (c) => c.memory.role == 'repailer');
+     
     utilities.deleteDeadCreeps();//this deletes the dead creeps that remains in the memory
 
-    if (numcreeps >= Object.keys(Game.creeps).length) {//this generates a creep in every spawn
-        utilities.generateCreep(roles[Math.floor((Math.random() * roles.length))]);
-        
+    if(minNumberOfBuilders>numberOfBuilders){
+        utilities.generateCreep(roles[0]);
     }
-
-
-    var tower = Game.getObjectById('1ff27ccb771c1fd');//tomo el objeto torre
-    if (tower) {
+    if(minNumberOfHarvesters>numberOfHarvesters){
+        utilities.generateCreep(roles[1]);
+    }
+    if(minNumberOfUpgraders>numberOfUpgraders){
+        utilities.generateCreep(roles[2]);
+    }
+    if(minNumberOfRepailers>numberOfRepailers){
+        utilities.generateCreep(roles[3]);
+    }
+   // if (numcreeps >= Object.keys(Game.creeps).length) {//this generates a creep in every spawn
+//        utilities.generateCreep(roles[Math.floor((Math.random() * roles.length))]);
+        
+  //  }
+  
+  
+    var towers = Game.rooms.W8N3.find(FIND_STRUCTURES, {
+        filter: (s) => s.structureType == STRUCTURE_TOWER
+    })
+    
+    //tower method
+    
+    for (let tower of towers){
+         if (tower) {
         var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
             filter: (structure) => structure.hits < structure.hitsMax
         });
@@ -35,7 +58,11 @@ module.exports.loop = function () {
             tower.attack(closestHostile);
         }
     }
+    }
 
+
+   
+    
 
     for (var name in Game.creeps) {
         var creep = Game.creeps[name];

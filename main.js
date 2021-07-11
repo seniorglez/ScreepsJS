46589b88681b261618219d87f906e6e44f38dedc
Diego Domínguez González <d.dominguez.glez1111@gmail.com>
2021-07-12 00:01:12 +0200

    Added a new condition in order to create a builder

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
 var roleRepailer = require('role.repailer');
 var roleClaimer = require('role.claimer');
+var roleFighter = require('role.fighter');
 var utilities = require('utilities');
 var config = require('config');
 
@@ -18,6 +19,7 @@ function operateTower(targetRoom) {
             var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
                 filter: (structure) => structure.hits < structure.hitsMax
             });
+            
             if (closestDamagedStructure) {
                 tower.repair(closestDamagedStructure);
             }
@@ -32,15 +34,22 @@ function operateTower(targetRoom) {
 
 function controlCreepPopulation(targetRoom) {
     
+    const numberOfConstructionSites = getNumberOfConstructionSites(targetRoom);
+    
     var numberOfBuilders =  _.sum( targetRoom.find(FIND_MY_CREEPS), (c) => c.memory.role == 'builder');
     var numberOfHarvesters = _.sum( targetRoom.find(FIND_MY_CREEPS), (c) => c.memory.role == 'harvester');
     var numberOfUpgraders = _.sum(targetRoom.find(FIND_MY_CREEPS), (c) => c.memory.role == 'upgrader');
     var numberOfRepailers = _.sum(targetRoom.find(FIND_MY_CREEPS), (c) => c.memory.role == 'repailer');
     
-    if (config.population.minNumberOfBuilders > numberOfBuilders) utilities.generateCreep(roles[0]);
+    if (config.population.minNumberOfBuilders > numberOfBuilders && numberOfConstructionSites > 0) utilities.generateCreep(roles[0]);
     if (config.population.minNumberOfHarvesters > numberOfHarvesters) utilities.generateCreep(roles[1]);
     if (config.population.minNumberOfUpgraders > numberOfUpgraders) utilities.generateCreep(roles[2]);
     if (config.population.minNumberOfRepailers > numberOfRepailers) utilities.generateCreep(roles[3]);
+}
+
+function getNumberOfConstructionSites(targetRoom) {
+    const construction_sites = targetRoom.find(FIND_CONSTRUCTION_SITES);
+    return construction_sites.length;
 }
 
 function operateCreeps() {
--- role.fighter.js
+++ role.fighter.js
@@ -10,5 +10,5 @@ var roleFighter = {
     }
 }
 module.exports = {
-
+    roleFighter
 };

78578c3b540d755d03250de594df2a6db262a986
Diego Dominguez <d.dominguez.glez1111@gmail.com>
2021-12-25 23:40:28 +0000

    module refactor

---
+++ creepManager.js
@@ -0,0 +1,39 @@
+var roleHarvester = require('role.harvester');
+var roleUpgrader = require('role.upgrader');
+var roleBuilder = require('role.builder');
+var roleRepailer = require('role.repailer');
+var roleClaimer = require('role.claimer');
+var roleFighter = require('role.fighter');
+
+const execute = (room) => {
+    console.log('Operating creeps in ' + room);
+    const creeps = room.find(FIND_MY_CREEPS);
+    creeps.forEach(element => {
+        console.log('Operating creep ' + element);
+        switch (element.memory.role) {
+            case 'harvester':
+                roleHarvester(element);
+                break;
+            case 'upgrader':
+                roleUpgrader(element);
+                break;
+            case 'builder':
+                roleBuilder(element);
+                break;
+            case 'repailer':
+                roleRepailer(element);
+                break;
+            case 'claimer':
+                roleClaimer(element);
+                break;
+            case 'fighter':
+                roleFighter(element);
+                break;
+            default:
+                console.log('WTF u doing')
+
+        }
+    });
+}
+
+module.exports = { execute };
--- role.harvester.js
+++ role.harvester.js
@@ -2,29 +2,23 @@
  * This use case allows you make creeps work as harvesters.
  **/
 
-var roleHarvester = {
-
-    /**
-     * Executes de use case.
-     *  @param {Creep} creep **/
-    run: function (creep) {
-        if (creep.carry.energy < creep.carryCapacity) {
-            var sources = creep.room.find(FIND_SOURCES);
-            if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0], { visualizePathStyle: { stroke: '#ffaa00' } });
+const roleHarvester = (creep) => {
+    if (creep.carry.energy < creep.carryCapacity) {
+        var sources = creep.room.find(FIND_SOURCES);
+        if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(sources[0], { visualizePathStyle: { stroke: '#ffaa00' } });
+        }
+    } else {
+        var targets = creep.room.find(FIND_STRUCTURES, {
+            filter: (structure) => {
+                return (structure.structureType == STRUCTURE_EXTENSION ||
+                    structure.structureType == STRUCTURE_SPAWN ||
+                    structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
             }
-        } else {
-            var targets = creep.room.find(FIND_STRUCTURES, {
-                filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_EXTENSION ||
-                        structure.structureType == STRUCTURE_SPAWN ||
-                        structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
-                }
-            });
-            if (targets.length > 0) {
-                if (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], { visualizePathStyle: { stroke: '#ffffff' } });
-                }
+        });
+        if (targets.length > 0) {
+            if (creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(targets[0], { visualizePathStyle: { stroke: '#ffffff' } });
             }
         }
     }
